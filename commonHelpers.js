521d72e01e51e0da1df39a3da1fa9bd0e6d59406
Toni777D <39744158+Toni777D@users.noreply.github.com>
2024-05-15 17:45:28 +0000

    Deploying to gh-pages from @ Toni777D/goit-js-hw-11@5b98c200837ba0ce9e76b29cc8b217e5a3f84f15 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,19 +1,19 @@
-import"./assets/vendor-86291ea8.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&a(l)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function a(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const i="https://pixabay.com/api/",n="43896740-362a21d10e9d41ec216c05f15",m=r=>{const s=new URLSearchParams({key:n,q:r,image_type:photo,orientation:horizontal,safesearch:!0});return fetch(`${i}?${s}`).then(o=>(o.ok||iziToast.error({title:"Sorry",message:"there are no images matching your search query. Please try again!"}),o.json()))},u=r=>r.map(({webformatURL:s,largeImageURL:o,tags:a,likes:e,views:t,comments:l,downloads:c})=>`
+import{S as m,i}from"./assets/vendor-0fc460d7.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&l(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function l(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const u=t=>t.map(({webformatURL:s,largeImageURL:o,tags:l,likes:e,views:r,comments:a,downloads:n})=>`
   <li class="gallery-item">
   <a class="gallery-link" href="${o}">
     <img
       class="gallery-image"
       src="${s}"
-      alt="${a}"
+      alt="${l}"
     />
   </a>
     <div class="small-content">
         <small class="text-body-likes">Likes: ${e}</small>
-        <small class="text-body-views">Views: ${t}</small>
-        <small class="text-body-comments">Comments: ${l}</small>
-        <small class="text-body-downloads">Dowloads: ${c}</small>
+        <small class="text-body-views">Views: ${r}</small>
+        <small class="text-body-comments">Comments: ${a}</small>
+        <small class="text-body-downloads">Dowloads: ${n}</small>
     </div>
  
     </li>s
-`).join(""),d=document.querySelector(".js-gallery");document.querySelector(".js-search-form");document.querySelector(".js-loader");m().then(r=>{d.innerHTML=u(r.hits)}).catch(r=>console.log(r));
+`).join("");new m(".gallery a",{captionsData:"alt",captionDelay:250});const d="https://pixabay.com/api/",y="43896740-362a21d10e9d41ec216c05f15",f=t=>{const s=new URLSearchParams({key:y,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${d}?${s}`).then(o=>(o.ok||i.error({title:"Sorry",message:"there are no images matching your search query. Please try again!"}),o.json()))},c=document.querySelector(".js-gallery"),h=document.querySelector(".js-search-form");document.querySelector(".js-loader");function p(t){t.preventDefault(),t.target.elements.searchKeyword.value.trim()===""&&(c.innerHTML="",t.target.reset(),i.error({title:"Error",message:"Illegal operation",position:"topRight",timeout:2e3}))}c.innerHTML="";f(searchQuery).then(t=>{c.innerHTML=u(t.results),console.log(t)}).catch(t=>console.log(t));h.addEventListener("submit",p);
 //# sourceMappingURL=commonHelpers.js.map
